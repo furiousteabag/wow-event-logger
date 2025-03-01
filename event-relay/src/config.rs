ad2829a1a1326f3958be6d350be65dc374a8185b
Alexander Smirnov <alex@asmirnov.xyz>
2025-02-28 20:49:59 -0500

    add input params as ENVs

---
+++ event-relay/src/config.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use std::env;
+
+pub type EnvVars = HashMap<&'static str, (&'static str, Option<&'static str>)>;
+
+pub fn init_env_vars() -> EnvVars {
+    let mut vars = HashMap::new();
+    vars.insert(
+        "WOWEVENTRELAY_WOW_FOLDER",
+        (
+            "Path to WoW installation folder",
+            Some("C:/Program Files (x86)/World of Warcraft"),
+        ),
+    );
+    vars.insert(
+        "WOWEVENTRELAY_EVENT_SERVER_URL",
+        (
+            "URL to send character updates",
+            Some("https://wow.asmirnov.xyz/character"),
+        ),
+    );
+    vars
+}
+
+pub fn get_env_value(vars: &EnvVars, key: &str) -> String {
+    let (_, default) = vars.get(key).expect("Unknown environment variable");
+    env::var(key).unwrap_or_else(|_| default.map(|v| v.to_string()).unwrap_or_default())
+}
+
+pub fn format_env_vars_help(vars: &EnvVars) -> String {
+    let mut help = String::from("\x1b[1;4mEnvironment Variables:\x1b[0m\n");
+    let mut keys: Vec<&&'static str> = vars.keys().collect();
+    keys.sort();
+    for &var_name in keys {
+        let (description, default_value) = vars.get(var_name).unwrap();
+        let var_desc = match default_value {
+            Some(value) => format!("{} (default: {})", description, value),
+            None => description.to_string(),
+        };
+        help.push_str(&format!("      {:<32} {}\n", var_name, var_desc));
+    }
+    if help.ends_with('\n') {
+        help.pop();
+    }
+    help
+}
--- event-relay/src/main.rs
+++ event-relay/src/main.rs
@@ -1,28 +1,33 @@
+mod config;
 mod logger;
 mod networking;
 mod parser;
 mod watcher;
 
-use clap::Parser;
-
-/// Monitor EventWatcher.lua for changes and send updates to the server
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Path to WoW installation folder
-    wow_folder: String,
-
-    /// URL to send character updates
-    #[arg(
-        short = 'u',
-        long = "event-server-url",
-        default_value = "https://wow.asmirnov.xyz/character"
-    )]
-    event_server_url: String,
-}
+use clap::Command;
 
 fn main() {
     logger::init_custom_logger();
-    let args = Args::parse();
-    watcher::monitor_events(&args.wow_folder, &args.event_server_url);
+    let env_vars = config::init_env_vars();
+
+    let cmd = Command::new("event-watcher")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Monitor EventWatcher.lua for changes and send updates to the server")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("start")
+                .about("Start monitoring EventWatcher.lua for changes")
+                .after_help(config::format_env_vars_help(&env_vars)),
+        );
+
+    let matches = cmd.get_matches();
+    match matches.subcommand() {
+        Some(("start", _)) => {
+            let wow_folder = config::get_env_value(&env_vars, "WOWEVENTRELAY_WOW_FOLDER");
+            let event_server_url = config::get_env_value(&env_vars, "WOWEVENTRELAY_EVENT_SERVER_URL");
+            watcher::monitor_events(&wow_folder, &event_server_url);
+        }
+        _ => unreachable!("Exhausted list of subcommands"),
+    }
 }
