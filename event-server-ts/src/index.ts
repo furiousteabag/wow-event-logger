73ee49fb4210a7e441eeaf2ed283792444167533
Alexander Smirnov <alex@asmirnov.xyz>
2025-03-13 23:08:11 -0500

    update main example

--- event-server-ts/src/index.ts
+++ event-server-ts/src/index.ts
@@ -2,6 +2,7 @@ import { createServer, startServer } from "./api/server"
 import { populateCharacter } from "./battlenet/battlenet"
 import { notifyTelegramSubscribers } from "./bot/telegram/telegram"
 import { handleCharactersUpdates } from "./character/characterUpdate"
+import startWatchedCharacterUpdateScheduler from "./scheduler/scheduler"
 import type { Character } from "./types/character"
 import logger from "./utils/logger"
 
@@ -11,12 +12,11 @@ let character: Character = {
   realm: "Doomhowl",
   name: "Furiousuncle",
 }
-
 character = await populateCharacter(character)
-
 logger.info(`Character data: ${JSON.stringify(character)}`)
-
 await handleCharactersUpdates([character], [notifyTelegramSubscribers])
+
+// startWatchedCharacterUpdateScheduler(1)
 
 const app = createServer()
 startServer(app)
