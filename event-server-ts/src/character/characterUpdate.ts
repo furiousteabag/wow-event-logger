6909666eae4218b8168a4b933886bf98cfa4f3c6
Alexander Smirnov <alex@asmirnov.xyz>
2025-03-13 22:00:58 -0500

    add mock of character update logic

---
+++ event-server-ts/src/character/characterUpdate.ts
@@ -0,0 +1,18 @@
+import type { Character, CharactersEvents } from "../types/character"
+import logger from "../utils/logger"
+
+export async function handleCharactersUpdates(
+  characters: Character[],
+  notifySubscribers: ((events: CharactersEvents) => Promise<void>)[],
+): Promise<CharactersEvents> {
+  logger.info(`Handling updates for ${characters.length} character(s)...`)
+
+  const charactersEvents: CharactersEvents = {
+    level_ups: [],
+    deaths: [],
+  }
+
+  await Promise.all(notifySubscribers.map((notifyFn) => notifyFn(charactersEvents)))
+
+  return charactersEvents
+}
